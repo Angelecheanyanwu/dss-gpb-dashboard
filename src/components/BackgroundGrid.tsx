f3083920f10f3c5acc45d908af9a5194b9b044ce
Angel Echeanyanwu <somtoecheanyanwu@gmail.com>
2026-02-10 15:29:21 +0100

    new updates

--- src/components/BackgroundGrid.tsx
+++ src/components/BackgroundGrid.tsx
@@ -6,7 +6,7 @@ export function BackgroundGrid() {
   return (
     <div className="absolute inset-0 z-0 pointer-events-none">
       <motion.div 
-        className="absolute inset-0 bg-[linear-gradient(to_right,var(--grid-color)_1px,transparent_1px),linear-gradient(to_bottom,var(--grid-color)_1px,transparent_1px)] bg-[size:4rem_4rem]"
+        className="absolute inset-0 bg-[linear-gradient(to_right,var(--grid-color)_1.5px,transparent_1.5px),linear-gradient(to_bottom,var(--grid-color)_1.5px,transparent_1.5px)] bg-[size:4rem_4rem]"
         animate={{
           backgroundPosition: ["0rem 0rem", "4rem 4rem"]
         }}
